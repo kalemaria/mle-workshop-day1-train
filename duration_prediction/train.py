75937564fc9e02c836ff8d8eb24f99804342669a
Maria Salfer <maria_kalemanov@yahoo.de>
2024-01-25 22:09:27 +0000

    Use click and parametrize training and validation

--- duration_prediction/train.py
+++ duration_prediction/train.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 
 import pickle
+from datetime import date
 
+import click
 import pandas as pd
 
 from sklearn.feature_extraction import DictVectorizer
@@ -25,32 +27,62 @@ def read_dataframe(filename):
     
     return df
 
-df_train = read_dataframe('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-01.parquet')
-df_val = read_dataframe('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-02.parquet')
+def train(train_month: date, validation_month: date, model_output_path: str) -> None:
+    url_template = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet'
+    train_url = url_template.format(year=train_month.year, month=train_month.month)
+    val_url = url_template.format(year=validation_month.year, month=validation_month.month)
 
-pipeline = make_pipeline(
-    DictVectorizer(),
-    LinearRegression()
-)
+    df_train = read_dataframe(train_url)
+    df_val = read_dataframe(val_url)
 
-# Will use only 3 features fro the model
-categorical = ['PULocationID', 'DOLocationID']
-numerical = ['trip_distance']
+    pipeline = make_pipeline(
+        DictVectorizer(),
+        LinearRegression()
+    )
 
-train_dicts = df_train[categorical + numerical].to_dict(orient='records')
-val_dicts = df_val[categorical + numerical].to_dict(orient='records')
+    # Will use only 3 features fro the model
+    categorical = ['PULocationID', 'DOLocationID']
+    numerical = ['trip_distance']
 
-target = 'duration'
-y_train = df_train[target].values
-y_val = df_val[target].values
+    train_dicts = df_train[categorical + numerical].to_dict(orient='records')
+    val_dicts = df_val[categorical + numerical].to_dict(orient='records')
 
-pipeline.fit(train_dicts, y_train)
+    target = 'duration'
+    y_train = df_train[target].values
+    y_val = df_val[target].values
 
-y_pred = pipeline.predict(val_dicts)
+    pipeline.fit(train_dicts, y_train)
 
-rmse = mean_squared_error(y_val, y_pred, squared=False)
+    y_pred = pipeline.predict(val_dicts)
 
-print(f'rmse = {rmse}')
+    rmse = mean_squared_error(y_val, y_pred, squared=False)
 
-with open('../models/lin_reg.bin', 'wb') as f_out:
-    pickle.dump(pipeline, f_out)
+    print(f'rmse = {rmse}')
+
+    with open(model_output_path, 'wb') as f_out:
+        pickle.dump(pipeline, f_out)
+
+@click.command()
+@click.option('--train-month', required=True, help='Training month in YYYY-MM format')
+@click.option('--validation-month', required=True, help='Validation month in YYYY-MM format')
+@click.option('--model-output-path', required=True, help='Path where the trained model will be saved')
+def run(train_month, validation_month, model_output_path):
+    train_year, train_month = train_month.split('-')
+    train_year = int(train_year)
+    train_month = int(train_month)
+
+    val_year, val_month = validation_month.split('-')
+    val_year = int(val_year)
+    val_month = int(val_month)
+
+    train_month = date(year=train_year, month=train_month, day=1)
+    validation_month = date(year=val_year, month=val_month, day=1)
+
+    train(
+        train_month=train_month,
+        validation_month=validation_month,
+        model_output_path=model_output_path
+    )
+
+if __name__ == '__main__':
+    run()
